bc1f28e770d9f6113fb7e03c0984927559d2a4b9
leo-capvano <leocapuano0@gmail.com>
2022-01-03 22:41:01 +0100

    .

--- src/main/java/cloudcomputing/accessmonitor/portal/controller/WelcomeController.java
+++ src/main/java/cloudcomputing/accessmonitor/portal/controller/WelcomeController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WelcomeController {
 
-    @RequestMapping(value = "welcome", method = RequestMethod.GET)
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
     public String welcomePage() {
         System.out.println("called welcome controller");
         return "welcome";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexPage() {
+        System.out.println("called index controller");
+        return "index";
     }
 }
