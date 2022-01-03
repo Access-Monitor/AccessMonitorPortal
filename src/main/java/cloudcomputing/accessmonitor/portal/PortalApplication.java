4602ae9410466b5d026af1138140f6b086508f39
leo-capvano <leocapuano0@gmail.com>
2022-01-03 22:08:54 +0100

    .

--- src/main/java/cloudcomputing/accessmonitor/portal/PortalApplication.java
+++ src/main/java/cloudcomputing/accessmonitor/portal/PortalApplication.java
@@ -2,12 +2,18 @@ package cloudcomputing.accessmonitor.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortalApplication {
+public class PortalApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PortalApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
     }
-
 }
