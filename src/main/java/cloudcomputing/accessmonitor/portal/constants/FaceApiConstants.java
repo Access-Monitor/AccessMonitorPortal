40f8e2740b443b249853483999e9726f69a37a28
GerardoBrescia <g.brescia3@studenti.unisa.it>
2022-01-20 22:13:31 +0100

    Fix bug variabili d'ambiente invertite.

--- src/main/java/cloudcomputing/accessmonitor/portal/constants/FaceApiConstants.java
+++ src/main/java/cloudcomputing/accessmonitor/portal/constants/FaceApiConstants.java
@@ -3,8 +3,8 @@ package cloudcomputing.accessmonitor.portal.constants;
 public class FaceApiConstants {
 
 
-    public static final String FACEAPI_SUBSCRIPTION_KEY = System.getenv("FaceAPIEndpoint");
-    public static final String FACEAPI_ENDPOINT = System.getenv("FaceAPISubscriptionKey");
+    public static final String FACEAPI_SUBSCRIPTION_KEY = System.getenv("FaceAPISubscriptionKey");
+    public static final String FACEAPI_ENDPOINT = System.getenv("FaceAPIEndpoint");
     public static final String FACEAPI_PERSON_GROUP_NAME = "mainpersongroup";
 
 }
